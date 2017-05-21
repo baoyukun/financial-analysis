894289d6c6a64429a299aee4f74264ce5f68468f
yukun.bao <baoyukun1994@126.com>
2017-05-21 22:38:24 +0200

    Version 3.0
    Enrich the database as much as possible.

--- continuous cal/synthesis.py
+++ continuous cal/synthesis.py
@@ -1,11 +1,7 @@
 #coding=utf-8
 
 def conclusion(buyerList):
-
     with open(unicode('analyse/0汇总表.txt', 'utf-8'), 'w') as outFile:
-        i = 'Reset the file!'
-
-    with open(unicode('analyse/0汇总表.txt', 'utf-8'), 'a') as outFile:
         for i in buyerList:
             with open(unicode('analyse/总持仓-'+i+'.txt', 'utf-8'), 'r') as inFile:
                 for j in inFile:
